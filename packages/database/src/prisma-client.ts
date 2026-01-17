4566034871915a79d262ff701a0a201e771f1c47
SukunDev <sukundev32@gmail.com>
2026-01-17 19:57:04 +0700

    change prisma-client.ts

--- packages/database/src/prisma-client.ts
+++ packages/database/src/prisma-client.ts
@@ -1,37 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client";
-import { config } from "dotenv";
-import { resolve } from "node:path";
+import "dotenv/config";
 
-config({
-  path: resolve(__dirname, "../../../.env"),
-});
-
-if (!process.env.DB_NAME) {
-  throw new Error("DB_NAME is missing (loaded from root .env)");
-}
-if (!process.env.DB_USER) {
-  throw new Error("DB_USER is missing (loaded from root .env)");
-}
-if (!process.env.DB_PASSWORD && process.env.DB_PASSWORD !== "") {
-  throw new Error("DB_PASSWORD is missing (loaded from root .env)");
-}
-if (!process.env.DB_HOST) {
-  throw new Error("DB_HOST is missing (loaded from root .env)");
-}
-if (!process.env.DB_PORT) {
-  throw new Error("DB_PORT is missing (loaded from root .env)");
-}
-
-if (!process.env.DB_SSL) {
-  throw new Error("DB_SSL is missing (loaded from root .env)");
-}
-
-const connectionString = `postgresql://${process.env.DB_USER}:${
-  process.env.DB_PASSWORD
-}@${process.env.DB_HOST}:${process.env.DB_PORT}/${
-  process.env.DB_NAME
-}?sslmode=${process.env.DB_SSL === "true" ? "require" : "disable"}`;
+const connectionString = process.env.DATABASE_URL;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
